e7a4c1b4c8768cca7763ff57236936352d385296
“Wendel” <wendelnunes9999@gmail.com>
2025-05-02 11:14:10 -0300

    feat: add search page configuration

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect, render, get_object_or_404
+from django.db.models import Q
 from contact.models import Contact
 from django.http import Http404
 # Create your views here.
@@ -19,8 +20,34 @@ def index(request):
         context
     )
     
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+    
+    if search_value == '':
+        return redirect('contact:index')
+    
+    print(search_value)
+    
+    contacts = Contact.objects\
+        .filter(show=True)\
+        .filter(
+            Q(first_name__icontains=search_value) |
+            Q(last_name__icontains=search_value)
+        )\
+        .order_by('-id')
+    
+    context = {
+        'contacts': contacts,
+        'site_title': 'Search -'
+    }
+    
+    return render(
+        request,
+        'contact/index.html',
+        context
+    )    
+    
 def contact(request, contact_id):
-    # single_contact = Contact.objects.filter(pk=contact_id).first()
     single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
     
     contact_name = f'{single_contact.first_name} {single_contact.last_name} - '
