f3791caff5026b0d8de248d71bb4a58f45ab0b66
“Wendel” <wendelnunes9999@gmail.com>
2025-05-05 19:49:06 -0300

    feat(auth): add login and logout pages

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,21 +1,20 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib import messages, auth
+from django.contrib.auth.forms import AuthenticationForm
 
 from contact.forms import RegisterForm
 # Create your views here.
 
 def register(request):
     form = RegisterForm()
-    
-    messages.info(request, 'Um texto qualquer')
-    
+        
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         
         if form.is_valid():
             form.save()
             messages.success(request, 'Usuário registrado')
-            return redirect('contact:index')
+            return redirect('contact:login')
     
     return render(
         request,
@@ -24,3 +23,29 @@ def register(request):
             'form': form 
         }
     )
+    
+def login_view(request):
+    form = AuthenticationForm(request)
+    
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        
+        if form.is_valid():
+            user = form.get_user()
+            auth.login(request, user)    
+            messages.success(request, 'Logado com suecesso!')
+            return redirect('contact:index')
+        
+        messages.error(request, 'Logado inválido!')
+        
+    return render(
+        request,
+        'contact/login.html',
+        {
+            'form': form 
+        }
+    )
+    
+def logout_view(request):
+    auth.logout(request)
+    return redirect('contact:login')
